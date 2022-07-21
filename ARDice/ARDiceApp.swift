080e69f71e74080c93bd458ccfe823f4e9c0da7f
Saadoun Alerris <saadoun@protonmail.com>
2022-07-21 14:24:59 +0300

    Initial Commit

---
+++ ARDice/ARDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARDiceApp.swift
+//  ARDice
+//
+//  Created by Saadoun Alerris on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ARDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
